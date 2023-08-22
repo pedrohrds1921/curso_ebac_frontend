996033b6925b0301553b7addef2bc7c3d3553eb0
Pedro Rodrigues <pedro.contate@gmail.com>
2023-08-21 23:28:34 -0300

    function with typescrpit

---
+++ src/ex_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+function mulply(numberOne, numberTwo) {
+    return numberOne * numberTwo;
+}
+function sayHello(name) {
+    return `Ola ${name}`;
+}
+console.log(mulply(2, 6));
+console.log(sayHello("pedro"));
---
+++ src/ex_ts.ts
@@ -0,0 +1,12 @@
+function mulply(numberOne:number,numberTwo:number):number{
+   
+    return numberOne*numberTwo
+}
+
+function sayHello(name:string):string{
+
+ return `Ola ${name}`
+}
+
+console.log(mulply(2,6))
+console.log(sayHello("pedro"))
